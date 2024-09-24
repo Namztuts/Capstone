9f9bbceea63cfab49c55b575be741f5540718f9a
Jacob S <me@mydomain.com>
2024-09-23 20:28:33 -0500

    Committing step one, project ideas, for my first Capstone Project!

---
+++ step1.py
@@ -0,0 +1,17 @@
+#Initial Project Ideas
+
+'''
+- write a short blurb explaining each idea; at a high level, describe the problem and the data i would use to solve it | highlight which one i would most like
+
+Calendar app | Google Calendar API
+A "simple" calendar app that allows users to create/edit/view their calendar and events on a day/week/month view
+A Login ability and the ability to export to Google calendar would be great additions
+
+Movie app | OMDb API | others will be needed more than likely
+Where it shows which streaming service has which movie
+Login to allow bookmarked movies/shows
+When researching, a challenge might be APIs limiting how many searches | suggested looking into caching | could limit results to a few?
+
+E-commerce app | FakeStore API
+Allow users to browse products and add to their shopping cart
+'''
